35e06929b65b09c3325f40b2ec19a5cbd7d3e034
Omar Ismail <omar_som3a2015@hotmail.com>
2022-09-06 14:03:28 +0200

    NN works, but the accuracy is very sensitive to the alpha, bigger data set might show if it really works or not

--- Algorithms/NN/Single-Hidden-Layer.py
+++ Algorithms/NN/Single-Hidden-Layer.py
@@ -39,25 +39,23 @@ X = df.drop(["Name", "Ticket", "Cabin", "Embarked", "PassengerId"], axis=1)
 X.replace({'male': 1, 'female': 0}, inplace=True)
 X = preprocess(X)
 y = np.array(df["Survived"]).reshape(X.shape[0], -1)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 class NN:
 
-    def __init__(self, hidden_layers, output_layer,learning_rate=0.001,max_iter=1000):
+    def __init__(self, hidden_layers, output_layer,learning_rate=0.001,max_iter=1000, reg_constant = 0.001):
         self.max_iter = max_iter
         self.hidden_layers = hidden_layers
         self.output_layer = output_layer
         self.learning_rate = learning_rate
-
-    # activation function
+        self.reg_constant = reg_constant
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x.copy()))
 
     def dsigmoid(self, x):
-        return self.sigmoid(x) * (1 - self.sigmoid(x))
+        return self.sigmoid(x.copy()) * (1 - self.sigmoid(x.copy()))
 
-    # loss function
     def loss(self, y, y_hat):
         return 1 / 2 * (y - y_hat) ** 2
 
@@ -104,13 +102,9 @@ class NN:
         return db
 
     def backward(self, y):
-        dc = (self.A[len(self.A) - 1] - y) * 2
+        dc = (self.A[len(self.A) - 1].copy() - y) * 2
         tmp = dc.copy()
         for i in range(self.hidden_layers + 1):
-            # tmp *= self.A[len(self.A) - (i + 1)]
-            da = self.dsigmoid(self.Z[len(self.Z) - (i + 1)])
-            dz = self.A[len(self.A) - (i + 2)]
-
             if i == 0:
                 db = self.dsigmoid(self.Z[-(1+i)]) * dc
                 dw= np.dot(self.A[-(2+i)].T,db)
@@ -118,17 +112,11 @@ class NN:
                 db = np.dot(self.dsigmoid(self.Z[-(1+i)]),np.dot(self.W[-i][1:],dw.T))
                 dw = np.dot(self.A[-(2+i)].T,db)
 
-            # print("db",db.shape)
-            # print("dw",dw.shape)
-            # print("Z",self.dsigmoid(self.Z[-(1+i)]).shape)
-            # # print("A", (self.A[-(2 + i)]).shape)
-            # print("W", self.W[-(1+i)].shape)
             db = self.avg_bias(db)
-            # print(db)
-            # print(i,self.W[-(1+i)].shape,dw.shape,dw.shape)
-            t = self.W[-(1+i)].copy()
-            self.W[-(1+i)][0] = self.W[-(1+i)][0] - db*(self.learning_rate/X.shape[0])
-            self.W[-(1+i)][1:] = self.W[-(1+i)][1:] - dw*(self.learning_rate/X.shape[0])
+            reg1 = self.W[-(1 + i)][0].copy()
+            reg2 = self.W[-(1+i)][1:].copy()
+            self.W[-(1+i)][0] = self.W[-(1+i)][0] - (db + np.sqrt(np.power(reg1,2).sum()) * self.reg_constant)*(self.learning_rate/X.shape[0])
+            self.W[-(1+i)][1:] = self.W[-(1+i)][1:] - (dw + np.sqrt(np.power(reg2,2).sum()) * self.reg_constant )*(self.learning_rate/X.shape[0])
 
     def train(self,X,y,batch_size=100,):
         for i in range(self.max_iter):
@@ -142,41 +130,32 @@ class NN:
 
     def predict(self,X):
         A = []
-        A.append(X)
+        A.append(X.copy())
         X = np.hstack((np.ones([X.shape[0], 1]), X))
         Z = []
-        Z.append(X @ self.W[0])
+        Z.append(np.dot(X, self.W[0]))
         A.append(self.sigmoid(Z[0]))
-        # print(Z[0].shape,X.shape,self.W[0].shape)
         for i in range(self.hidden_layers):
             matrix = self.W[i + 1]
-            tmp = A[len(A) - 1]
+            tmp = A[len(A) - 1].copy()
             tmp = np.hstack((np.ones([tmp.shape[0], 1]), tmp))
-            Z.append(tmp @ matrix)
+            Z.append(np.dot(tmp, matrix))
             A.append(self.sigmoid(Z[len(Z) - 1]))
-        # print(Z[len(Z) - 1])
         return A[-1]
 
-model = NN(hidden_layers=2, output_layer=1,learning_rate=0.001,max_iter=1000)
+model = NN(hidden_layers=2, output_layer=1,learning_rate=1,max_iter=1000,reg_constant= 0.01)
 model.layers(size=np.array([8,4]), input_size=X.shape[1])
-start = X.shape[0]
-t = X[0].reshape(-1, X.shape[1])
-t2 = X[0:start]
-ty = y[0]
-ty2 = y[0:start]
 # model.forward(t2, ty2)
-model.train(X,y)
-print(model.W)
-predicted = model.predict(X)
-print(predicted)
+model.train(X_train,y_train)
+# print(model.W)
+predicted = model.predict(X_test)
+
+predicted = np.where(predicted > 0.5, 1, 0)
 f = 0
-# for i in range (X.shape[0]):
-#     if predicted[i] > 0.5:
-#         val = 1
-#     else: val = 0
-#     if val != y[i]:
-#         f+= 1
-f = (predicted-y).sum()
-# print("score ", (1- f/X.shape[0]), " number of wrong " , f , "out of " , X.shape[0])
+for i in range(y_test.shape[0]):
+    if y_test[i] != predicted[i]:
+        f+=1
+print(predicted,y_test)
 print("score ", (1- f/X.shape[0]), " number of test cases" , X.shape[0])
+print(f)
 
