dcb4e0d6fb3d56a997c1a4392d4f734526de2629
Omar Ismail <omar_som3a2015@hotmail.com>
2022-09-11 12:20:47 +0200

    adding notes for andrews ML course

--- Algorithms/NN/Multi-Hidden-Layer.py
+++ Algorithms/NN/Multi-Hidden-Layer.py
@@ -1,4 +1,7 @@
-# under construction
+# missing features
+# bias can be removed
+# gradient checking
+# cross-validation test for lambda constant
 import sys
 import math
 import numpy as np
@@ -16,14 +19,6 @@ pd.set_option('display.max_rows', None)
 
 df = pd.read_csv("train.csv")
 
-
-# print(df.describe())
-# print(df.groupby('Survived').count()['PassengerId'])
-# corr = df.corr()
-# plt.figure(figsize=(11,8))
-# sns.heatmap(corr, cmap="BuPu",annot=True)
-# plt.show()
-
 # preprocessing
 def preprocess(X):
     X['Fare'].fillna(int(X['Fare'].mean()), inplace=True)
@@ -34,27 +29,26 @@ def preprocess(X):
     # print(X.shape)
     return X
 
-
 X = df.drop(["Name", "Ticket", "Cabin", "Embarked", "PassengerId"], axis=1)
 X.replace({'male': 1, 'female': 0}, inplace=True)
 X = preprocess(X)
 y = np.array(df["Survived"]).reshape(X.shape[0], -1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 class NN:
 
-    def __init__(self, hidden_layers, output_layer,learning_rate=0.001,max_iter=1000, reg_constant = 0.001):
+    def __init__(self, hidden_layers, output_layer,learning_rate=0.001,max_iter=1000, reg_constant = 0.001,bias= True):
         self.max_iter = max_iter
         self.hidden_layers = hidden_layers
         self.output_layer = output_layer
         self.learning_rate = learning_rate
         self.reg_constant = reg_constant
-
+        self.bias = bias
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x.copy()))
+        return 1 / (1 + np.exp(-x))
 
     def dsigmoid(self, x):
-        return self.sigmoid(x.copy()) * (1 - self.sigmoid(x.copy()))
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
 
     def loss(self, y, y_hat):
         return 1 / 2 * (y - y_hat) ** 2
@@ -68,11 +62,14 @@ class NN:
             return
         # creating weights for each layer
         W = []
+        np.random.seed(1234)
         W.append(np.random.rand(input_size + 1, size[0]))
         for i in range(len(size)):
+            np.random.seed(1235+i)
             if i == 0:
                 continue
             W.append(np.random.rand(size[i - 1] + 1, size[i]))
+        np.random.seed(12)
         W.append(np.random.rand(size[len(size) - 1] + 1, self.output_layer))
         self.W = W
         for i in range(len(W)):
@@ -80,15 +77,15 @@ class NN:
 
     def forward(self, X, y):
         A = []
-        A.append(X.copy())
-        X = np.hstack((np.ones([X.shape[0], 1]), X.copy()))
+        A.append(X)
+        X = np.hstack((np.ones([X.shape[0], 1]), X))
         Z = []
         Z.append(np.dot(X , self.W[0]))
-        A.append(self.sigmoid(Z[0]).copy())
+        A.append(self.sigmoid(Z[0]))
         # print(Z[0].shape,X.shape,self.W[0].shape)
         for i in range(self.hidden_layers):
             matrix = self.W[i + 1]
-            tmp = A[len(A) - 1].copy()
+            tmp = A[len(A) - 1]
             tmp = np.hstack((np.ones([tmp.shape[0], 1]), tmp))
             Z.append(np.dot(tmp, matrix))
             A.append(self.sigmoid(Z[len(Z) - 1]))
@@ -102,8 +99,7 @@ class NN:
         return db
 
     def backward(self, y):
-        dc = (self.A[len(self.A) - 1].copy() - y) * 2
-        tmp = dc.copy()
+        dc = (self.A[len(self.A) - 1] - y) * 2
         for i in range(self.hidden_layers + 1):
             if i == 0:
                 db = self.dsigmoid(self.Z[-(1+i)]) * dc
@@ -113,8 +109,8 @@ class NN:
                 dw = np.dot(self.A[-(2+i)].T,db)
 
             db = self.avg_bias(db)
-            reg1 = self.W[-(1 + i)][0].copy()
-            reg2 = self.W[-(1+i)][1:].copy()
+            reg1 = self.W[-(1 + i)][0]
+            reg2 = self.W[-(1+i)][1:]
             self.W[-(1+i)][0] = self.W[-(1+i)][0] - (db + np.sqrt(np.power(reg1,2).sum()) * self.reg_constant)*(self.learning_rate/X.shape[0])
             self.W[-(1+i)][1:] = self.W[-(1+i)][1:] - (dw + np.sqrt(np.power(reg2,2).sum()) * self.reg_constant )*(self.learning_rate/X.shape[0])
 
@@ -130,14 +126,14 @@ class NN:
 
     def predict(self,X):
         A = []
-        A.append(X.copy())
+        A.append(X)
         X = np.hstack((np.ones([X.shape[0], 1]), X))
         Z = []
         Z.append(np.dot(X, self.W[0]))
         A.append(self.sigmoid(Z[0]))
         for i in range(self.hidden_layers):
             matrix = self.W[i + 1]
-            tmp = A[len(A) - 1].copy()
+            tmp = A[len(A) - 1]
             tmp = np.hstack((np.ones([tmp.shape[0], 1]), tmp))
             Z.append(np.dot(tmp, matrix))
             A.append(self.sigmoid(Z[len(Z) - 1]))
@@ -145,17 +141,25 @@ class NN:
 
 model = NN(hidden_layers=2, output_layer=1,learning_rate=1,max_iter=1000,reg_constant= 0.01)
 model.layers(size=np.array([8,4]), input_size=X.shape[1])
-# model.forward(t2, ty2)
 model.train(X_train,y_train)
-# print(model.W)
-predicted = model.predict(X_test)
-
-predicted = np.where(predicted > 0.5, 1, 0)
+#test
+predicted = np.where( model.predict(X_test) > 0.5, 1, 0)
 f = 0
 for i in range(y_test.shape[0]):
     if y_test[i] != predicted[i]:
         f+=1
-print(predicted,y_test)
 print("score ", (1- f/X.shape[0]), " number of test cases" , X.shape[0])
 print(f)
+
+
+
+# Tips:
+# 1. You can usually prevent over-fitting if you keep your number of neurons below:
+#       size of the data set "rows" / alpha * ( size of the input layer + size of the output layer)
+# where alpha is number between 2 - 10
+# 2. Regularization technique:
+#       1.Dropout   2.L2,L1
+# 3. Optimization of the Network Configuration:
+#    1.pruning nodes
+
 
